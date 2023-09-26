eeb0f3f6293b0a932e651a6d4cd799258d91d352
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2023-09-26 17:06:32 +0100

    Added capacity for 2D colliders to Trigger by Script

--- Triggers/Scripts/TriggerBy.cs
+++ Triggers/Scripts/TriggerBy.cs
@@ -40,8 +40,22 @@ namespace ScottEwing.Triggers
             };
         }
         
+        
         //--is invalid if collision events should not be used or if not colliding with object in layer mask or with target tag
         public bool IsCollisionValid(Collision other, bool isActivatable = true) => IsColliderValid(other.collider, isActivatable);
         
+        public bool IsColliderValid(Collider2D other, bool isActivatable = true) => IsColliderValid(other.transform, isActivatable);
+        public bool IsColliderValid(Transform other, bool isActivatable = true) {
+            if (!isActivatable) {
+                return false;
+            }
+
+            return _triggeredBy switch {
+                TriggeredBy.Tag => other.CompareTag(_triggeredByTag),
+                TriggeredBy.LayerMask => _triggeredByMask.IsLayerInLayerMask(other.gameObject.layer),
+                TriggeredBy.Either => other.CompareTag(_triggeredByTag) || _triggeredByMask.IsLayerInLayerMask(other.gameObject.layer),
+                _ => false
+            };
+        }
     }
 }
