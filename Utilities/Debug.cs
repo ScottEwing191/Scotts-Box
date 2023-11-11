48428baa4f22cb4f62294c71750b517e4524c71d
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2023-11-11 15:20:54 +0000

    Rename Types Forlder To Utilites
    Added Debug Script

---
+++ Utilities/Debug.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace ScottEwing.Scotts_Box.Utilities{
+    public static class Debug{
+        public static bool DebugMode = true;
+        public static void Log(string message) {
+            if (DebugMode)
+                UnityEngine.Debug.Log(message);
+        }
+
+        public static void Log(string message, Object context) {
+            if (DebugMode)
+                UnityEngine.Debug.Log(message, context);
+        }
+
+        public static void LogWarning(string message) {
+            if (DebugMode)
+                UnityEngine.Debug.LogWarning(message);
+        }
+
+        public static void LogWarning(string message, Object context) {
+            if (DebugMode)
+                UnityEngine.Debug.LogWarning(message, context);
+        }
+
+        public static void LogError(string message) {
+            if (DebugMode)
+                UnityEngine.Debug.LogError(message);
+        }
+
+        public static void LogError(string message, Object context) {
+            if (DebugMode)
+                UnityEngine.Debug.LogError(message, context);
+        }
+
+    }
+}
