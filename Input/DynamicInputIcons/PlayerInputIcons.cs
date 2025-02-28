cc26285904a27631f19e190afe9ccf5679e173d6
scott.ewing <scott.ewing62@gmail.com>
2025-02-28 19:16:23 +0000

    Tidied up player input icons component. Added option to control interaction text based on control scheme. Can also select control scheme to select in editor
    Added mouse button / mouse scroll direction icons

--- Input/DynamicInputIcons/PlayerInputIcons.cs
+++ Input/DynamicInputIcons/PlayerInputIcons.cs
@@ -10,13 +10,16 @@ namespace ScottEwing.Input.DynamicInputIcons{
     }
     
     public class PlayerInputIcons : MonoBehaviour{
+        
+        
         [SerializeField] protected PlayerInput _playerInput;
         private InputBinding _inputBindingMask;
         private ControllerInputTypes _types;
 
         private UiInputIcon[] iconsActiveOnstart; 
 
-
+        public PlayerInput PlayerInput() => _playerInput;
+        
         private void Awake() {
             _playerInput ??= GetComponent<PlayerInput>();
         }
@@ -50,25 +53,38 @@ namespace ScottEwing.Input.DynamicInputIcons{
         public void SetUpIcons(PlayerInput playerInput) {
             if (playerInput.devices.Count == 0) return;
             var controllerName = playerInput.devices[0].name;
+            var controlScheme = playerInput.currentControlScheme;
 
+            //foreach (var scheme in playerInput.actions.controlSchemes) {
+            //    Debug.Log(scheme.name);
+            //}
+
+            
+            
             if (controllerName == "DualSenseGamepadHID") {
-                _inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                //_inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                _inputBindingMask = InputBinding.MaskByGroup(controlScheme);
                 _types = ControllerInputTypes.PS5Controller;
             }
             else if (InputSystem.IsFirstLayoutBasedOnSecond(controllerName, "DualShockGamepad")) {
-                _inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                //_inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                _inputBindingMask = InputBinding.MaskByGroup(controlScheme);
+
                 _types = ControllerInputTypes.PS4Controller;
             }
             else if (InputSystem.IsFirstLayoutBasedOnSecond(controllerName, "Gamepad")) {
-                _inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                //_inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                _inputBindingMask = InputBinding.MaskByGroup(controlScheme);
                 _types = ControllerInputTypes.XboxController;
             }
             else if (InputSystem.IsFirstLayoutBasedOnSecond(controllerName, "Keyboard")) {
-                _inputBindingMask = InputBinding.MaskByGroup("Keyboard&Mouse");
+                //_inputBindingMask = InputBinding.MaskByGroup("Keyboard&Mouse");
+                _inputBindingMask = InputBinding.MaskByGroup(controlScheme);
                 _types = ControllerInputTypes.KeyboardMouse;
             }
             else {
-                _inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                //_inputBindingMask = InputBinding.MaskByGroup("GamePad");
+                _inputBindingMask = InputBinding.MaskByGroup(controlScheme);
                 _types = ControllerInputTypes.XboxController;
             }
 
--- Input/DynamicInputIcons/UiInputIcon.cs
+++ Input/DynamicInputIcons/UiInputIcon.cs
@@ -24,14 +24,7 @@ namespace ScottEwing.Input.DynamicInputIcons{
             public ControllerInputTypes _inputType;
             public float _scale = 1.0f;
         }
-
-        /*[Serializable]
-        private class InputDeviceIsCompositePair{
-            public ControllerInputTypes _inputDevice;
-            public bool _isPartOfComposite;
-        }*/
-
-
+        
         private enum CompositeType{
             Positive,
             Negative,
@@ -40,7 +33,11 @@ namespace ScottEwing.Input.DynamicInputIcons{
             Left,
             Right,
         }
-
+        
+        private enum NoIconFoundBehaviour{ DisableImage, DisableGameObject }
+        
+        // --- Input Actions ---
+        [Title("Input Actions")]
         [HideIf("_assignActionPerControlType")]
         [SerializeField] protected InputActionReference _actionReference;
 
@@ -48,43 +45,112 @@ namespace ScottEwing.Input.DynamicInputIcons{
             get => _actionReference;
             set => _actionReference = value;
         }
-
+        
+        [SerializeField] private NoIconFoundBehaviour _noIconFoundBehaviour = NoIconFoundBehaviour.DisableImage;
+        
         [SerializeField] private bool _assignActionPerControlType;
         [ShowIf("_assignActionPerControlType")]
         [SerializeField] private List<KeyValuePair> _controlTypeActions = new List<KeyValuePair>();
-
-
-        [Space]
+        
+        // --- Input Type Scaling ---
+        [Title("Input Type Scaling")]
         [SerializeField] private List<InputDeviceScalePair> _inputTypeScale;
 
 
-        [Space]
-        [Header("Composite Bindings")]
+        // --- Composite Bindings ---
+        [Title("Composite Bindings")]
         [SerializeField] private bool _usingCompositeBindings;
 
         [ShowIf("_usingCompositeBindings")]
         [SerializeField] private CompositeType _compositeType;
-
+        
         [ShowIf("_usingCompositeBindings")]
-        [SerializeField] private List<ControllerInputTypes> _compositeControlTypes;
+        [SerializeField] private List<ControllerInputTypes> _nonCompositeControlTypes;
+        
+        // --- Interaction Text ---  
+        [Title("Interaction Text")]
+        [ValueDropdown(nameof(GetControlSchemes))]
+        [SerializeField] private List<string> _interactionTextControlSchemes;
+        [SerializeField] private TextMeshProUGUI _interactionText;
+        
 
-
-        [Header("Text Instead of Image")]
+        //[Header("Text Instead of Image")]
+        // --- Text Instead of Image ---
+        [Title("Text Instead of Image")]
         [SerializeField] private List<ControllerInputTypes> _usesTextInsteadOfImage;
+        [PropertySpace(0, 10)] // Adds 10 pixels after this field
         [SerializeField] private TextMeshProUGUI _inputText;
-
+        
+        [FoldoutGroup("Input Icons")]
         [SerializeField] private InputIconsSprites _keyboardIcons;
+        [FoldoutGroup("Input Icons")]
         [SerializeField] private InputIconsSprites _ps4Icons;
+        [FoldoutGroup("Input Icons")]
         [SerializeField] private InputIconsSprites _xboxIcons;
+        [FoldoutGroup("Input Icons")]
         [SerializeField] private InputIconsSprites _ps5Icons;
+        [FoldoutGroup("Input Icons")]
         [SerializeField] private Image _image;
-
-
+        
+        // --- Editor Settings (Grouped at the Bottom) ---
+        //[Title("Editor Settings")]
         [Space]
+        [BoxGroup("Editor Settings")]
+        [ValueDropdown(nameof(GetControlSchemes))]
+        public string selectedControlScheme;
+        [BoxGroup("Editor Settings")]
         [Tooltip("This is the control type which will be used in the editor. Does not effect the icon used when the game is run")]
         [SerializeField] private ControllerInputTypes _editorType = ControllerInputTypes.KeyboardMouse;
 
+        [BoxGroup("Editor Settings")]
+        [Button]
+        public void SetIconInEditor()
+        {
+            _image ??= GetComponent<Image>();
 
+            InputBinding inputBindingMask;
+
+            if (!string.IsNullOrEmpty(selectedControlScheme))
+            {
+                // Use the selected control scheme
+                inputBindingMask = InputBinding.MaskByGroup(selectedControlScheme);
+                print("inputBindingMask: " + inputBindingMask);
+            }
+            else {
+                // Fall back to the default mapping based on the editor type
+                inputBindingMask = _editorType switch {
+                    ControllerInputTypes.KeyboardMouse => InputBinding.MaskByGroup("Keyboard&Mouse"),
+                    ControllerInputTypes.PS4Controller => InputBinding.MaskByGroup("GamePad"),
+                    ControllerInputTypes.XboxController => InputBinding.MaskByGroup("GamePad"),
+                    ControllerInputTypes.PS5Controller => InputBinding.MaskByGroup("GamePad"),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+
+            SetImageSprite(_editorType, inputBindingMask);
+        }
+        
+        private IEnumerable<string> GetControlSchemes()
+        {
+            // Find a valid action reference
+            var actionRef = _actionReference;
+            if (actionRef == null)
+            {
+                // Look for the first non-null action in the collection
+                actionRef = _controlTypeActions.FirstOrDefault(action => action != null)?._value;
+            }
+            
+            
+            if (actionRef != null && actionRef.asset != null)
+            {
+                yield return "";
+                foreach (var scheme in actionRef.asset.controlSchemes)
+                {
+                    yield return scheme.name;
+                }
+            }
+        }
+        
         private Dictionary<CompositeType, string> _compositeTypeNames = new Dictionary<CompositeType, string> {
             { CompositeType.Positive, "positive" },
             { CompositeType.Negative, "negative" },
@@ -93,19 +159,6 @@ namespace ScottEwing.Input.DynamicInputIcons{
             { CompositeType.Left, "left" },
             { CompositeType.Right, "right" },
         };
-
-        [Button]
-        public void SetIconInEditor() {
-            _image ??= GetComponent<Image>();
-            var inputBindingMask = _editorType switch {
-                ControllerInputTypes.KeyboardMouse => InputBinding.MaskByGroup("Keyboard&Mouse"),
-                ControllerInputTypes.PS4Controller => InputBinding.MaskByGroup("GamePad"),
-                ControllerInputTypes.XboxController => InputBinding.MaskByGroup("GamePad"),
-                ControllerInputTypes.PS5Controller => InputBinding.MaskByGroup("GamePad"),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            SetImageSprite(_editorType, inputBindingMask);
-        }
 
         private void Awake() {
             _image ??= GetComponent<Image>();
@@ -120,13 +173,21 @@ namespace ScottEwing.Input.DynamicInputIcons{
             if (_inputText) {
                 _inputText.gameObject.SetActive(false);
             }
-
-            _image.enabled = true;
-
+            
+            
+            
+            // Activate Image or GameObject
+            SetImageOrGameObjectActive(true);
+            _image.enabled = true;  // ensure image is enabled (may have been disable independently of SetImageOrGameObjectActive by SetTextNotImage)
+            
 
             var actionReference = GetActionReference(types);
-            if (actionReference == null) {
-                _image.enabled = false;
+            
+            Debug.Log("Interaction: " + actionReference.action.interactions, this);
+            
+            if (!actionReference) {
+                //_image.enabled = false;
+                SetImageOrGameObjectActive(false);
                 return;
             }
 
@@ -134,12 +195,18 @@ namespace ScottEwing.Input.DynamicInputIcons{
             var displayStringOptions = InputBinding.DisplayStringOptions.DontIncludeInteractions;
             int bindingIndex = GetBindingIndex(types, mask, actionReference);
             if (bindingIndex == -1) {
-                _image.enabled = false;
+                //_image.enabled = false;
+                SetImageOrGameObjectActive(false);
                 return;
             }
+            
+            //-- Display Interaction Text
+            SetInteractionText(mask, actionReference, bindingIndex);
 
             var bindingDisplay = actionReference.action.GetBindingDisplayString(bindingIndex, out var deviceLayoutName, out var controlPath, displayStringOptions);
-
+            
+            
+            
             if (_usesTextInsteadOfImage.Contains(types)) {
                 SetTextNotImage(bindingDisplay);
                 return;
@@ -153,8 +220,7 @@ namespace ScottEwing.Input.DynamicInputIcons{
 
                 _ => throw new ArgumentOutOfRangeException(nameof(types), types, null)
             };
-
-
+            
             SetImageScale(types);
         }
 
@@ -162,22 +228,19 @@ namespace ScottEwing.Input.DynamicInputIcons{
         /// Sets the scale of the image to 1 unless the input type is in the inputTypeScale list in which case it will set the scale to the value in the list.
         /// </summary>
         private void SetImageScale(ControllerInputTypes types) {
-            float scale = 1.0f;
+            var scale = 1.0f;
             var scaleEntry = _inputTypeScale.Find(x => x._inputType == types);
             if (scaleEntry != null) {
                 scale = scaleEntry._scale;
             }
-
             _image.rectTransform.localScale = Vector3.one * scale;
         }
-
-
+        
         private void SetTextNotImage(string text) {
             _image.enabled = false;
             _inputText.SetText("" + text + "");
             _inputText.gameObject.SetActive(true);
         }
-
 
         private InputActionReference GetActionReference(ControllerInputTypes types) {
             if (!_assignActionPerControlType) {
@@ -191,15 +254,14 @@ namespace ScottEwing.Input.DynamicInputIcons{
             }
 
             return null;
-            //return controlTypeActions.ContainsKey(types) ? controlTypeActions[types] : null;
         }
 
-
         private int GetBindingIndex(ControllerInputTypes types, InputBinding mask, InputActionReference actionReference) {
-            if (_usingCompositeBindings && _compositeControlTypes.Contains(types)) {
+            //if (_usingCompositeBindings && _compositeControlTypes.Contains(types)) {
+            if (_usingCompositeBindings && !_nonCompositeControlTypes.Contains(types)) {
                 return GetCompositeBindingIndex(actionReference.action, mask, _compositeType);
             }
-
+            
             return actionReference.action.GetBindingIndex(mask);
         }
         
@@ -216,6 +278,48 @@ namespace ScottEwing.Input.DynamicInputIcons{
                 }
 
             return -1;
+        }
+
+        private void SetInteractionText(InputBinding inputBindingMask, InputActionReference actionReference, int bindingIndex) {
+            _interactionText.gameObject.SetActive(false);
+            
+            if (!_interactionTextControlSchemes.Contains(inputBindingMask.groups))
+                return;
+
+            var binding = actionReference.action.bindings[bindingIndex];
+            var interaction = string.IsNullOrEmpty(binding.interactions) ? actionReference.action.interactions : binding.interactions;
+
+            if (string.IsNullOrEmpty(interaction))
+                return;
+
+            _interactionText.SetText(interaction.Split(',')[0]);
+            _interactionText.gameObject.SetActive(true);
+            
+            /*if (!_interactionTextControlSchemes.Contains(inputBindingMask.groups)) 
+                return;
+            var bindingInteractions = actionReference.action.bindings[bindingIndex].interactions;
+            if (string.IsNullOrEmpty(bindingInteractions)) {
+                bindingInteractions = actionReference.action.interactions;
+            }
+
+            if (string.IsNullOrEmpty(bindingInteractions) ) {
+                return;
+            }
+            
+            bindingInteractions = bindingInteractions.Split(',', StringSplitOptions.None)[0];
+            
+            _interactionText.SetText(bindingInteractions);
+            _interactionText.gameObject.SetActive(true);
+            */
+            
+        }
+        
+        private void SetImageOrGameObjectActive(bool active) {
+            if (_noIconFoundBehaviour == NoIconFoundBehaviour.DisableImage) {
+                _image.enabled = active;
+            }else if (_noIconFoundBehaviour == NoIconFoundBehaviour.DisableGameObject) {
+                gameObject.SetActive(active);
+            }
         }
     }
 }
